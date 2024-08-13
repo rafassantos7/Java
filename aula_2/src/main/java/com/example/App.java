7947346a3a5a4f16ff8af4699017952597cdbde1
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2024-08-13 13:31:12 +0000

    Up

---
+++ aula_2/src/main/java/com/example/App.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    Scanner ler = new Scanner(System.in);
+    
+    float media, primeiraNota, segundaNota;
+
+    System.out.print("Digite sua primeira nota: ");
+    primeiraNota = ler.nextFloat();
+    
+    System.out.print("Digite sua segunda nota: ");
+    segundaNota = ler.nextFloat();
+
+    media = (primeiraNota + segundaNota)/2;
+
+    System.out.println("Media final: " + media);
+
+    if (media >=7 ){
+        System.out.println("Voce foi aprovado.");
+    }
+    else if (media > 5 && media <6.9){
+        System.out.println("Verificacao suplementear");
+    }
+    else if (media<5){
+        System.out.println("Recuperacao");
+    }
+    ler.close();
+    }
+}
